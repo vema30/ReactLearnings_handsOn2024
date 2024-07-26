c74899f409879d6aa6b5a8b4e73253a94b67fd7b
vema30 <119170582+vema30@users.noreply.github.com>
2024-07-26 12:32:30 +0530

    Initial commit

---
+++ my-react-app/src/Info.jsx
@@ -0,0 +1,13 @@
+
+
+function Info(props)
+{ 
+     <div>
+        <p>Name : {props.fname}</p>
+        <p>Name : {props.lname}</p>
+        <p>My favorite car brand : {props.favbrand}</p>
+        <p>is it cheacked {props.ischecked}</p>
+        <p>Gender selected :</p>
+        Price:
+     </div>
+}
